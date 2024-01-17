524064924717c53a0bf4e10aea9ec5609be7f5d2
brenda-soto <bsoto@pursuit.org>
2024-01-17 11:05:40 -0500

    Components organized

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import StudentCard from "./StudentCard";
 
 const StudentList = () => {
-  return <div>StudentList</div>;
+  return (
+    <div>
+      <StudentCard />
+    </div>
+  );
 };
 
 export default StudentList;
