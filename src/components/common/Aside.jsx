a3ac3f67764eba9f53c0c5de29c9be5c75973156
brenda-soto <bsoto@pursuit.org>
2024-01-18 20:21:41 -0500

    created cohortList with api data

--- src/components/common/Aside.jsx
+++ src/components/common/Aside.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
 const Aside = ({ students }) => {
-  
-//   const cohortList = students.map(student => {
-//   return   student.cohort.cohortCode
-//  });
+  const fullCohortList = students.map((student) => {
+    return student.cohort.cohortCode;
+  });
 
-//  const deletedDuplicatesCohortList
-
-  return (<div>
-  
-  </div>)
+  const cohortList = [...new Set(fullCohortList)];
+  console.log(cohortList);
+  return (
+    <div>
+      <h3>Choose a Class By Start Date</h3>
+      <button>Sort {} By Year</button>
+      <section>
+        {/* {cohortList.map(cohort => {
+        const 
+      })} */}
+      </section>
+    </div>
+  );
 };
 
 export default Aside;
