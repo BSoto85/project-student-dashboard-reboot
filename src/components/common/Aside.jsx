f49bec2ead93ad06604d446c40930f3303ae0ae7
brenda-soto <bsoto@pursuit.org>
2024-01-22 11:13:01 -0500

    deleted extra div

--- src/components/common/Aside.jsx
+++ src/components/common/Aside.jsx
@@ -1,49 +1,23 @@
-import { useState, useEffect } from "react";
+import React from "react";
 
-const Aside = ({ students, handleOnClick }) => {
-  const [orderStatus, setOrderStatus] = useState(false);
-  const [sortOrder, setSortOrder] = useState("Descending");
-  const [orderedList, setOrderedList] = useState([]);
-  const [cohortList, setCohortList] = useState([]);
+const Aside = ({ students }) => {
+  const fullCohortList = students.map((student) => {
+    return student.cohort.cohortCode;
+  });
 
-  const sort = (cohortList) => {
-    if (orderStatus === true) {
-      const ascendingCohortList = cohortList.toSorted();
-      setSortOrder("Descending");
-      setOrderedList(ascendingCohortList);
-    } else {
-      console.log("else cohortlist", cohortList);
-      const descendingCohortList = cohortList.toSorted().reverse();
-      setSortOrder("Ascending");
-      setOrderedList(descendingCohortList);
-    }
-    setOrderStatus(!orderStatus);
-    return sortOrder;
-  };
-
-  useEffect(() => {
-    if (students.length > 0) {
-      const fullCohortList = students.map((student) => {
-        return student.cohort.cohortCode;
-      });
-      const uniqueCohorts = [...new Set(fullCohortList)];
-      sort(uniqueCohorts);
-      setCohortList(uniqueCohorts);
-    }
-  }, [students]);
+  const cohortList = [...new Set(fullCohortList)];
 
   return (
-    <div className="float-end" style={{ width: "350px", marginRight: "20px"}}>
+    <div>
       <h3>Choose a Class By Start Date</h3>
-      <button onClick={() => sort(cohortList)}>Sort {sortOrder} By Year</button>
+      <button>Sort {} By Year</button>
       <ul>
-        {orderedList.map((cohort) => {
+        {cohortList.map((cohort) => {
           const cohortSeason = cohort.slice(0, cohort.length - 4);
           const cohortYear = cohort.slice(-4);
-          const formattedCohort = `${cohortSeason} ${cohortYear}`;
           return (
-            <li onClick={() => handleOnClick(cohort)} key={cohort} className="list-group-item list-group-item list-group-item-primary">
-              {formattedCohort}
+            <li key={cohort}>
+              {cohortSeason} {cohortYear}
             </li>
           );
         })}
